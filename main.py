d18efe00b92f4563df87817de457d30305d0a198
jumho01270127-hash <jumho01270127@gmail.com>
2025-11-04 14:06:22 +0900

    Fix formatting of warning message in main.py

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ name=st.text_input('이름을 말해주세요')
 st.selectbox('좋아하는 음식을 선택하시기',['재은','우주','응호'])
 if st.button('인사생성'):
   st.info(name+'안녕하시기')
-  st.warning(menu+'을(를)종아하시기,나는 별로')
+  st.warning(menu+'을(를) 종아하시기,나는 별로')
   st.error('반갑수다')
