fe02fd0346656896a788573532bae8e89cda1dec
jumho01270127-hash <jumho01270127@gmail.com>
2025-11-04 13:59:05 +0900

    Add food selection feature to web service

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 st.title('나의 첫 웹 서비스 만들기!')
 name=st.text_input('이름을 말해주세요')
+st.selectbox('좋아하는 음식을 선택하시기',['재은','우주','응호'])
 if st.button('인사생성'):
   st.write(name+'안녕하시기')
 
