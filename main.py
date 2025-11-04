1c42262496dca7f66193b30e39da475ed5d63b57
jumho01270127-hash <jumho01270127@gmail.com>
2025-11-04 14:05:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ st.title('나의 첫 웹 서비스 만들기!')
 name=st.text_input('이름을 말해주세요')
 st.selectbox('좋아하는 음식을 선택하시기',['재은','우주','응호'])
 if st.button('인사생성'):
-  st.write(name+'안녕하시기')
-
+  st.info(name+'안녕하시기')
+  st.warning(menu+'을(를)종아하시기 나는 별로')
+  st.error('반갑수다')
